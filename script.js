e006febd9308143ec98088be9180703b8236d5dc
Zellrei <anthonylaurentweb@gmail.com>
2022-01-20 15:46:20 +0100

    Update script.js
    fully functionnal game with 5 rounds

--- script.js
+++ script.js
@@ -1,54 +1,96 @@
-// function that returns random choice from computer OK
 function computerPlay () {
   const choicesArray = ['rock', 'paper', 'scissors'];
   const computerSelection = choicesArray[(Math.random() * choicesArray.length) | 0];
   return(computerSelection);
 }
 
-// const playerSelection = 'rock';
-// let computerSelection = computerPlay();
 
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
+  let winner;
   let result;
-
   console.log(`You chose : ${playerSelection}`);
   console.log(`Computer chose : ${computerSelection}`);
 
   if (playerSelection === computerSelection) {
+    winner = 'Noone';
     result = `It's a tie !`
+  } else if (playerSelection === 'rock') {
+      if (computerSelection === 'paper') {
+        winner = 'computer';
+        result = `Computer wins ! ${computerSelection} beats ${playerSelection}`;
+      } else {
+        winner = 'player';
+        result = `You win ! ${playerSelection} beats ${computerSelection}`;
+    }
+  } else if (playerSelection === 'paper') {
+      if (computerSelection === 'scissors') {
+        winner = 'computer';
+        result = `Computer wins ! ${computerSelection} beats ${playerSelection}`;
+      }
+      else {
+        winner = 'player';
+        result = `You win ! ${playerSelection} beats ${computerSelection}`;
+    }
+  } else if (playerSelection === 'scissors') {
+      if (computerSelection === 'rock') {
+        winner = 'computer';
+        result = `Computer wins ! ${computerSelection} beats ${playerSelection}`;
+      }
+      else {
+        winner = 'player';
+        result = `You win ! ${playerSelection} beats ${computerSelection}`;
+      } 
+    }
+  else {
+    winner = 'Noone';
+    result = `${playerSelection} is not a valid choice.`;
   }
-  else if (playerSelection === 'rock') {
-    if (computerSelection === 'paper')
-      result = `Computer wins ! ${computerSelection} beats ${playerSelection}`;
-    else
-      result = `You win ! ${playerSelection} beats ${computerSelection}`;
-  }
-  else if (playerSelection === 'paper') {
-    if (computerSelection === 'scissors')
-      result = `Computer wins ! ${computerSelection} beats ${playerSelection}`;
-    else
-      result = `You win ! ${playerSelection} beats ${computerSelection}`;
-  }
-  else if (playerSelection === 'scissors') {
-    if (computerSelection === 'rock')
-      result = `Computer wins ! ${computerSelection} beats ${playerSelection}`;
-    else
-      result = `You win ! ${playerSelection} beats ${computerSelection}`;
-  } else result = `${playerSelection} is not a valid choice.`
 
-  return result;
+  console.log(result);
+  return winner;
 }
   
-console.log(playRound ('Rock', computerPlay()));
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  let gameWinner = 'Noone';
+  let gameResults = '';
 
+  for (let index = 0; index < 5; index++) {
+    
+    let playerSelection = prompt(`Rock, Paper, Scissors ?`, `Rock`);
+    if (typeof playerSelection != 'string') playerSelection = 'None';
+    console.log(`Round ${index + 1}`);
+    let roundWinner = playRound (playerSelection, computerPlay());
+    
+    if (roundWinner === 'player') {
+      playerScore++;
+    } else if (roundWinner === 'computer') {
+      computerScore++;
+    } 
 
-// input from player : R, P or S (case insensitive) (LATER)
+    console.log(`Player score is : ${playerScore}`);
+    console.log(`Computer score is : ${computerScore}`);
+    console.log('');
+  }
 
-// if playerSelection === computerSelection then tie
-// else return a string for winning or losing + what happened (e.g. You win ! Paper beats rock)
+  if (playerScore > computerScore) {
+      gameWinner = 'player';
+      gameResults = `Player won with ${playerScore} points !`;
+  } else if (playerScore < computerScore) {
+      gameWinner = 'computer';
+      gameResults = `Computer won with ${computerScore} points !`;
+  } else { 
+      gameWinner = 'Noone';
+      gameResults = `Nobody wins, it's a draw !`;
+  }
 
+  console.log(gameResults);
+  return gameWinner;
+}
 
+game();
 
 
 
